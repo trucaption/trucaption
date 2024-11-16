3870e89d2d51428d20a3da864e93a478ef3bffde
Derek Kaser <11674153+dkaser@users.noreply.github.com>
2024-11-16 02:53:28 +0000

    ci: add timestamp

--- packages/server/electron-builder.js
+++ packages/server/electron-builder.js
@@ -12,6 +12,8 @@ const config = {
       endpoint: process.env.AZURE_SIGN_ENDPOINT,
       codeSigningAccountName: process.env.AZURE_SIGN_ACCOUNT_NAME,
       certificateProfileName: process.env.AZURE_CERT_PROFILE_NAME,
+      TimestampRfc3161: "http://timestamp.acs.microsoft.com",
+      TimestampDigest: "SHA256",
     },
   },
   nsis: {
